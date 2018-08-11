62514c63f6d96e3202e86930c6bee675defecc7c
Luiza Engler Stadelhofer <luiza.engler@gmail.com>
2018-08-10 23:12:25 -0300

    Added wall mechanic

---
+++ Closing In/Closing In/Assets/Scripts/Wall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	    InvokeRepeating("ChangePosition", 0, 5); //calls ChangePosition() every 2 secs
+ 	}
+ 
+ 	void ChangePosition () {
+    	transform.position = transform.position + new Vector3(1, 0, 0);
+   	}
+}
---
+++ Closing In/Closing In/Assets/Scripts/Wall2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	    InvokeRepeating("ChangePosition", 0, 5); //calls ChangePosition() every 2 secs
+ 	}
+ 
+ 	void ChangePosition () {
+    	//Compute position for next time
+    	transform.position = transform.position + new Vector3(-1, 0, 0);
+ 	}
+}
