f5136ff165b10b726427c60ca8af24249a5535b1
luizaes <luiza.engler@gmail.com>
2020-01-02 20:30:11 -0300

    Add initial files to Planetary Guardian

---
+++ Planetary Guardian/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+
+    public Transform transform;
+    public Collider2D collider;
+    private GameObject planet;
+    private int side;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        planet = GameObject.Find("Planet");
+
+        side = (int) Random.Range(0, 4);
+
+        if(side == 1)
+        {
+            Vector3 position = new Vector3(Random.Range(-14f, 14f), 8f, 0f);
+            transform.position = position;
+        } else if(side == 2)
+        {
+            Vector3 position = new Vector3(15f, Random.Range(-6f, 6f), 0f);
+            transform.position = position;
+        } else if(side == 3)
+        {
+            Vector3 position = new Vector3(Random.Range(-14f, 14f), -8f, 0f);
+            transform.position = position;
+        } else
+        {
+            Vector3 position = new Vector3(-15f, Random.Range(-6f, 6f), 0f);
+            transform.position = position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Planetary Guardian/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Collider2D collider;
+    public Transform transform;
+    public GameObject planet;
+    private float radius = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = planet.transform.position.z - Camera.main.transform.position.z;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector3 direction = mousePosition - planet.transform.position;
+        direction = Vector3.ClampMagnitude(direction, radius);
+
+        if ((double)direction.sqrMagnitude < (double)radius * (double)radius)
+        {
+            direction = direction.normalized * radius;
+        }
+
+        transform.position = planet.transform.position + direction;
+
+    }
+}
